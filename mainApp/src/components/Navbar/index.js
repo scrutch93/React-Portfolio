e8453586a1f0735f243e724f3811d877dd3265c5
Steven Crutchfield <stevccru@icloud.com>
2021-08-18 23:35:48 -0400

    Updated changes, adding in hamburger menu

--- mainApp/src/components/Navbar/index.js
+++ mainApp/src/components/Navbar/index.js
@@ -3,12 +3,19 @@ import {Link} from 'react-router-dom';
 import "./navstyle.css"
 
 const Navbar = () => {
+    const [click, setClick] =useState (false);
+
     return (
         <nav className= "navbar">
-            <div className ="navbar"></div>
+            <div className ="navbar-container">
             <Link to="/" className="navbar -logo">
             Steven Crutchfield
             </Link>
+            <div className ="menu-icon" onClick={handleClick}>
+                <i className= {click ? 'fas fa-times' : 'fas fa-bars'}/>
+
+            </div>
+            </div>
         </nav>
     )
 }
