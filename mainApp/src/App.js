731520f1c50c19fcf4e87f1d8104f3f8884092ba
Steven Crutchfield <stevccru@icloud.com>
2021-08-18 20:32:48 -0400

    "more changes maded to app.js"

--- mainApp/src/App.js
+++ mainApp/src/App.js
@@ -1,21 +1,26 @@
 import {useState} from "react"
 import "./style.css"
 import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Switch, Route }from 'react-router-dom';
+
 import Text from "./components/Text";
 import Input from "./components/Input";
 
 function App() {
-  const [text, setText] = useState("Steven")
+  //const [text, setText] = useState("Steven")
   
-  const update = (event) => {
-    setText(event.target.value)
-  }
+  //const update = (event) => {
+   // setText(event.target.value)
+  //}
 
   return (
     <>
+    <Router>
       <Navbar />
-      <Text mirror={text}/>
-      <Input change={update}/>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
     </>
   );
 }
--- mainApp/src/components/Navbar/index.js
+++ mainApp/src/components/Navbar/index.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom';
 import "./navstyle.css"
 
 const Navbar = () => {
     return (
         <nav className= "navbar">
             <div className ="navbar"></div>
-            <Link to="/" className="navbar -logo"></Link>
+            <Link to="/" className="navbar -logo">
+            Steven Crutchfield
+            </Link>
         </nav>
     )
 }
